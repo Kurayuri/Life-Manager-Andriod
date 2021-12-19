896929743cd4b3992ed235b020719d390e45db8f
Kurayuri <Kurayuri@Kurayuri-PC05>
2021-12-19 11:48:28 +0800

    Fix bill audit barchart markerView

--- app/src/main/java/com/trashparadise/lifemanager/ui/bills/XYMarkerView.java
+++ app/src/main/java/com/trashparadise/lifemanager/ui/bills/XYMarkerView.java
@@ -7,26 +7,30 @@ import android.widget.TextView;
 import com.github.mikephil.charting.components.MarkerView;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.utils.MPPointF;
 import com.trashparadise.lifemanager.R;
 
 import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
 
 @SuppressLint("ViewConstructor")
 public class XYMarkerView extends MarkerView {
 
     private final TextView tvContent;
-//    private final IAxisValueFormatter xAxisValueFormatter;
+    private final ValueFormatter xAxisValueFormatter;
 
     private final DecimalFormat format;
+    private final SimpleDateFormat simpleDateFormat;
 
-    public XYMarkerView(Context context, IAxisValueFormatter xAxisValueFormatter) {
+    public XYMarkerView(Context context, ValueFormatter xAxisValueFormatter) {
         super(context, R.layout.markar_view);
 
-//        this.xAxisValueFormatter = xAxisValueFormatter;
+        this.xAxisValueFormatter = xAxisValueFormatter;
         tvContent = findViewById(R.id.tvContent);
         format = new DecimalFormat(context.getResources().getString(R.string.amount_decimal_format));
+        simpleDateFormat=new SimpleDateFormat(context.getResources().getString(R.string.date_format_day));
     }
 
     // runs every time the MarkerView is redrawn, can be used to update the
@@ -34,8 +38,7 @@ public class XYMarkerView extends MarkerView {
     @Override
     public void refreshContent(Entry e, Highlight highlight) {
 
-//        tvContent.setText(String.format("x: %s, y: %s", xAxisValueFormatter.getFormattedValue(e.getX(), null), format.format(e.getY())));
-        tvContent.setText(String.format(format.format(e.getY())));
+        tvContent.setText(String.format("%s: %s",xAxisValueFormatter.getFormattedValue(e.getX()),format.format(e.getY())));
 
         super.refreshContent(e, highlight);
     }
